7f728200fd8d8e27bae959db4ca029d48c05ecf4
Saitoh Koki <koki0702@gmail.com>
2015-10-11 11:03:54 +0900

    Update youtube.py

--- intro/youtube.py
+++ intro/youtube.py
@@ -1,6 +1,6 @@
 import json
 from urllib.request import urlopen
-url = "https://raw.githubusercontent.com/koki0702/introducing-python/dummy_api/youTube_top_rated.json"
+url = "https://raw.githubusercontent.com/koki0702/introducing-python/master/dummy_api/youTube_top_rated.json"
 response = urlopen(url)
 contents = response.read()
 text = contents.decode('utf8')
